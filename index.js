f6660782bb280a65df962516957346cfed3d1d8e
Icaro Caldeira <icaro.caldeira@hotmail.com>
2018-05-07 17:59:01 -0300

    fix chat format

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
+let clients = [];
+
 io.on('connection', (client) => {
+  clients.push(client.id);
+
+  client.join('general');
+
   client.on('message:toServer', (data) => {
     io.emit('message:toClient', data)
+  });
+
+  client.on('message:toGeneral', (data) => {
+    io.to('general').emit('message:toGeneralClient');
+  });
+
+  client.on('disconnect', () => {
+    const index = clients.indexOf(client.id);
+
+    clients.splice(index, 1);
   });
 })
 
